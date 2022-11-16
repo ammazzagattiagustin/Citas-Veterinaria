b49dfbb895f5eb3568f9eee4f47e470fddf90378
Agustin <agus.ammazzagatti@gmail.com>
2022-11-16 19:13:52 -0300

    firt commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"], // va a buscar todos los archivos que tengan extensión .jsx en src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
